7d5581fa7db860de0c0929ce9a176f2ce0a0c761
Perva_Simona <perva.simona@yahoo.com>
2023-12-13 15:48:46 +0200

    fisier

--- data/script.js
+++ data/script.js
@@ -1,19 +1,40 @@
 const API_KEY = "6bfc30471a0731f63221660b6f2ba7af";
-const cityForm = document.querySelector("#cityForm");
+const API_URL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
+const searchBox = document.querySelector(".search input");
+const searchButton = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
-function fetchData(city = 'Timisoara') {
-    const API_URL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid= ${API_KEY}`;
+async function checkWeather(city) {
+    const response = await fetch(API_URL + city + `&appid=${API_KEY}`);
+    const data = await response.json()
+    console.log(data);
+  
+    document.querySelector(".city").innerHTML = data.name;
+    const tempCelcius = Math.round(data.main.temp);
+    document.querySelector(".temp").innerHTML = tempCelcius + " °C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
-    fetchData(API_URL)
-        .then((response) => response.json())
-        .then((responseJSON) => console.log(responseJSON));
+    if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = "./data/img/clouds.png";
+    }
+    else if (data.weather[0].main == "Clear") {
+        weatherIcon.src = "./data/img/clear.png";
+    }
+    else if (data.weather[0].main == "Rain") {
+        weatherIcon.src = "./data/img/rain.png";
+    }
+    else if (data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "./data/img/drizzle.png";
+    }
+    else if (data.weather[0].main === "Mist") {
+        weatherIcon.src = "./data/img/mist.png";
+    }
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".err").style.display = "none";
 }
-function cityFormSubmitHandler(event) {
-    
-    const data = new FormData(event.target)
-    const entries = Object.fromEntries(data.entries());
-    
-    fetchData(entriescity)
-}
-cityForm.addEventListener("submit", (event) => cityFormSubmitHandler(event));
+searchButton.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+})
+checkWeather();
